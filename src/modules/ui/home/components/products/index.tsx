cb6d2a406316e3ad9f1d4ab86d84030e9275c621
Mohmed-Ayoub-Js <clanning852@gmail.com>
2025-03-22 14:57:40 +0100

    add any

--- src/modules/ui/home/components/products/index.tsx
+++ src/modules/ui/home/components/products/index.tsx
@@ -3,7 +3,7 @@ import { trpc } from "@/utils/trpc";
 import ClientProducts from "./client/client-products";
 
 const ProductsPage = () => {
-  const { data: products, isLoading, error } = trpc.getAllProduct.getAllProducts.useQuery();
+  const { data: products, isLoading, error } : any= trpc.getAllProduct.getAllProducts.useQuery();
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
