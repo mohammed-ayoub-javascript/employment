3321279b4d17ac9cfe5385f01f1a7d15c81c8de6
Mohmed-Ayoub-Js <clanning852@gmail.com>
2025-03-22 14:55:52 +0100

    q-f

--- src/modules/ui/home/components/products/index.tsx
+++ src/modules/ui/home/components/products/index.tsx
@@ -1,8 +1,12 @@
-import { trpcServer } from "@/utils/trpc-server";
+"use client";
+import { trpc } from "@/utils/trpc";
 import ClientProducts from "./client/client-products";
 
-const ProductsPage = async () => {
-  const products  : any = await trpcServer.getAllProduct.getAllProducts.query();
+const ProductsPage = () => {
+  const { data: products, isLoading, error } = trpc.getAllProduct.getAllProducts.useQuery();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return <ClientProducts initialProducts={products} />;
 };
